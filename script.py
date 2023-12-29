f1164aecebdef07e191e61eda3f17b95b3ea1dd3
ArmaanBarak <armaanbarak@outlook.com>
2023-12-29 23:05:06 +0530

    Add vertical check in check_for_win() method. Fix modify grid break statement. Add test statements for check_for_win().

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ class Grid:
         for row in self.grid[::-1]:
             if row[col_num - 1] == "_":
                 row[col_num - 1] = symbol
-            break
+                break
 
     def check_for_win(self, symbol):
         # horizontal 4-match check
@@ -34,14 +34,24 @@ class Grid:
                 return True
             elif ((row[3] == symbol) and (row[4] == symbol) and (row[5] == symbol) and (row[6] == symbol)):
                 return True
+        
+        # vertical 4-match check
+        for col_index in range(7):
+            if ((self.grid[0][col_index] == symbol) and (self.grid[1][col_index] == symbol) and (self.grid[2][col_index] == symbol) and (self.grid[3][col_index] == symbol)):
+                return True
+            elif ((self.grid[1][col_index] == symbol) and (self.grid[2][col_index] == symbol) and (self.grid[3][col_index] == symbol) and (self.grid[4][col_index] == symbol)):
+                return True
+            elif ((self.grid[2][col_index] == symbol) and (self.grid[3][col_index] == symbol) and (self.grid[4][col_index] == symbol) and (self.grid[5][col_index] == symbol)):
+                return True
+        
 
 my_grid = Grid()
 
 # testing for check_for_win() method
 my_grid.modify_grid(3, "X")
-my_grid.modify_grid(4, "X")
-my_grid.modify_grid(2, "X")
-my_grid.modify_grid(5, "X")
+my_grid.modify_grid(3, "X")
+my_grid.modify_grid(3, "X")
+my_grid.modify_grid(3, "X")
 my_grid.show_grid()
 # should print True
 print(my_grid.check_for_win("X"))
