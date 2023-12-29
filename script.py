e26b50455e7547ea65001fde64e3cfb0eb90719d
ArmaanBarak <armaanbarak@outlook.com>
2023-12-29 15:39:27 +0530

    Add check_for_win() method to check for horizontal match and test method.

--- script.py
+++ script.py
@@ -9,7 +9,6 @@ class Grid:
                      ["_", "_", "_", "_", "_", "_", "_"],
                      ["_", "_", "_", "_", "_", "_", "_"]]
 
-
     def show_grid(self):
         for row in self.grid:
 
@@ -24,9 +23,25 @@ class Grid:
                 row[col_num - 1] = symbol
             break
 
+    def check_for_win(self, symbol):
+        # horizontal 4-match check
+        for row in self.grid[::-1]:
+            if ((row[0] == symbol) and (row[1] == symbol) and (row[2] == symbol) and (row[3] == symbol)):
+                return True
+            elif ((row[1] == symbol) and (row[2] == symbol) and (row[3] == symbol) and (row[4] == symbol)):
+                return True
+            elif ((row[2] == symbol) and (row[3] == symbol) and (row[4] == symbol) and (row[5] == symbol)):
+                return True
+            elif ((row[3] == symbol) and (row[4] == symbol) and (row[5] == symbol) and (row[6] == symbol)):
+                return True
+
 my_grid = Grid()
 
-# testing modify_grid() method
-# adding "X" in first empty space of column 3 from bottom
+# testing for check_for_win() method
 my_grid.modify_grid(3, "X")
+my_grid.modify_grid(4, "X")
+my_grid.modify_grid(2, "X")
+my_grid.modify_grid(5, "X")
 my_grid.show_grid()
+# should print True
+print(my_grid.check_for_win("X"))
