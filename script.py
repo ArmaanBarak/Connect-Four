ec2388d5ca24b71b78061c4299414328ab40155b
ArmaanBarak <armaanbarak@outlook.com>
2023-12-30 07:54:04 +0530

    Add Right to left diagnol check. Add test case for check_for_win() method. Fix no. of iterations in left to right check to prevent errors.

--- script.py
+++ script.py
@@ -45,7 +45,7 @@ class Grid:
                 return True
         
         # diagnol check left to right
-        for row_index in range(4):
+        for row_index in range(3):
             if ((self.grid[row_index][0] == symbol) and (self.grid[row_index + 1][1] == symbol) and (self.grid[row_index + 2][2] == symbol) and (self.grid[row_index + 3][3] == symbol)):
                 return True
             elif ((self.grid[row_index][1] == symbol) and (self.grid[row_index + 1][2] == symbol) and (self.grid[row_index + 2][3] == symbol) and (self.grid[row_index + 3][4] == symbol)):
@@ -55,21 +55,31 @@ class Grid:
             elif ((self.grid[row_index][3] == symbol) and (self.grid[row_index + 1][4] == symbol) and (self.grid[row_index + 2][5] == symbol) and (self.grid[row_index + 3][6] == symbol)):
                 return True
         
+        # Diagnol 4-match check right to left
+        for row_index in range(3):
+            if ((self.grid[row_index][-1] == symbol) and (self.grid[row_index + 1][-2] == symbol) and (self.grid[row_index + 2][-3] == symbol) and (self.grid[row_index + 3][-4] == symbol)):
+                return True
+            elif ((self.grid[row_index][-2] == symbol) and (self.grid[row_index + 1][-3] == symbol) and (self.grid[row_index + 2][-4] == symbol) and (self.grid[row_index + 3][-5] == symbol)):
+                return True
+            elif ((self.grid[row_index][-3] == symbol) and (self.grid[row_index + 1][-4] == symbol) and (self.grid[row_index + 2][-5] == symbol) and (self.grid[row_index + 3][-6] == symbol)):
+                return True
+            elif ((self.grid[row_index][-4] == symbol) and (self.grid[row_index + 1][-5] == symbol) and (self.grid[row_index + 2][-6] == symbol) and (self.grid[row_index + 3][-7] == symbol)):
+                return True
         
 
 my_grid = Grid()
 
 # testing for check_for_win() method
-my_grid.modify_grid(1, "X")
-my_grid.modify_grid(1, "O")
-my_grid.modify_grid(1, "O")
-my_grid.modify_grid(1, "X")
-my_grid.modify_grid(2, "O")
-my_grid.modify_grid(2, "O")
-my_grid.modify_grid(2, "X")
-my_grid.modify_grid(3, "O")
-my_grid.modify_grid(3, "X")
+my_grid.modify_grid(6, "X")
+my_grid.modify_grid(6, "O")
+my_grid.modify_grid(6, "O")
+my_grid.modify_grid(6, "X")
+my_grid.modify_grid(5, "O")
+my_grid.modify_grid(5, "O")
+my_grid.modify_grid(5, "X")
+my_grid.modify_grid(4, "O")
 my_grid.modify_grid(4, "X")
+my_grid.modify_grid(3, "X")
 my_grid.show_grid()
 
 # should print True
