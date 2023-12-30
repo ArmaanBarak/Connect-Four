d7c4b369ff4a0979336d6ff3e7255cfc468e242c
ArmaanBarak <armaanbarak@outlook.com>
2023-12-30 10:55:35 +0530

    Game Code Complete. Tests Done.

--- script.py
+++ script.py
@@ -66,21 +66,72 @@ class Grid:
             elif ((self.grid[row_index][-4] == symbol) and (self.grid[row_index + 1][-5] == symbol) and (self.grid[row_index + 2][-6] == symbol) and (self.grid[row_index + 3][-7] == symbol)):
                 return True
         
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
 
-my_grid = Grid()
 
-# testing for check_for_win() method
-my_grid.modify_grid(6, "X")
-my_grid.modify_grid(6, "O")
-my_grid.modify_grid(6, "O")
-my_grid.modify_grid(6, "X")
-my_grid.modify_grid(5, "O")
-my_grid.modify_grid(5, "O")
-my_grid.modify_grid(5, "X")
-my_grid.modify_grid(4, "O")
-my_grid.modify_grid(4, "X")
-my_grid.modify_grid(3, "X")
-my_grid.show_grid()
+if __name__ == "__main__":
+    print("Welcome to Connect-Four Game!")
+    player_1_name = input("Please enter your name Player 1: ")
+    player_2_name = input("Please enter your name Player 2: ")
 
-# should print True
-print(my_grid.check_for_win("X"))
+    while True:
+        player_1_symbol = input("{}, Choose your mark to represent your ball on the grid (Either 'X' or 'O'): ".format(player_1_name))
+        if player_1_symbol.capitalize() not in ['X', 'O']:
+            print("Invalid Input!")
+            continue
+        break
+
+    player_1_symbol = player_1_symbol.capitalize()
+
+    if player_1_symbol == "X":
+        player_2_symbol = "O"
+    else:
+        player_2_symbol = "X"
+
+    player1 = Player(player_1_name, player_1_symbol)
+    player2 = Player(player_2_name, player_2_symbol)
+    my_grid = Grid()
+
+    print("Here's the grid ->")
+    my_grid.show_grid()
+
+    print("{} will go first!".format(player1.name))
+
+    match_found = False
+
+    while True:
+
+        while True:
+            col_number = int(input("{} enter the column, where you'ld like to insert your ball (1-7): ".format(player1.name)))
+            if col_number not in [1, 2, 3, 4, 5, 6, 7]:
+                print("Invalid Input!")
+                continue
+            print("Inserting value!")
+            my_grid.modify_grid(col_number, player1.symbol)
+            break
+
+        match_found = my_grid.check_for_win(player1.symbol)
+        my_grid.show_grid()
+
+        if match_found:
+            print("Congratulations! {} you WON!!!".format(player1.name))
+            break
+
+        while True:
+            col_number = int(input("{} enter the column, where you'ld like to insert your ball (1-7): ".format(player2.name)))
+            if col_number not in [1, 2, 3, 4, 5, 6, 7]:
+                print("Invalid Input!")
+                continue
+            print("Inserting value!")
+            my_grid.modify_grid(col_number, player2.symbol)
+            break
+
+        match_found = my_grid.check_for_win(player1.symbol)
+        my_grid.show_grid()
+
+        if match_found:
+            print("Congratulations! {} you WON!!!".format(player1.name))
+            break
